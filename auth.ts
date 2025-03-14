3a145ba4cf14c2d89d2a5cd2b1caa255f63533fd
Gaurav kumar <indiagauravkumar@gmail.com>
2025-03-14 22:06:52 +0000

    added prisma

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
-
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import Google from "next-auth/providers/google"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+import { prisma } from "@/prisma"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
 })
